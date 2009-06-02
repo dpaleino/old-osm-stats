e6a2f7e5b38aa84229a14695154e41ded4fd4aac
David Paleino <d.paleino@gmail.com>
2009-06-02 22:39:57 +0200

    Adding script to import italy.osm from cron

---
+++ import_planet.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+url="http://download.geofabrik.de/osm/europe/italy.osm.bz2"
+today=$(date +%Y%m%d)
+output="italy_$today.osm"
+prefix="italy_$today"
+
+wget $url -O $output.bz2
+bunzip2 $output.bz2
+
+# Save a copy of the .bz2
+bzip2 $output
+[ -d archive ] || mkdir archive/
+mv $output.bz2 archive/
+
+# Import data into PostGIS
+osm2pgsql -l -p $prefix $output
+
+# Create the statistics table
+echo "CREATE TABLE osm_stat_$today AS \
+SELECT c.cod_reg, c.pro_com, l.highway, l.ref, l.name, intersection(l.way, transform(c.geom, 4326)) \
+FROM $prefix_line l, it_comuni c \
+WHERE l.highway <> '' AND l.way &&  transform(c.geom, 4326) AND intersects(l.way, transform(c.geom, 4326))" | \
+	psql -d gis
+
+if [ "$?" -eq 2 ]; then
+	mail --subject "Statistics table creation failed!" d.paleino@gmail.com
+fi
+# Remove uncompressed file -- we have the .bz2 one in archive/.
+rm $output
