8a6f5ef3d3b894762a84d829a3b193de52c081ab
David Paleino <d.paleino@gmail.com>
2009-09-07 23:02:46 +0200

    A bit of refactoring: save useful stats in a separate JSON file

--- parse.py
+++ parse.py
@@ -3,28 +3,38 @@
 
 import libxml2
 import sys
+import os
 
 from genshi.template import MarkupTemplate
 from collections import defaultdict
 from json import JsonReader, JsonWriter, ReadException
+from datetime import date
+
+stats_path = "stats.json"
+html_path = "statistiche.html"
+
+check = {
+    "highway": [
+        "bus_stop",
+        ],
+    "amenity": [
+        "drinking_water",
+        "pharmacy",
+        "bank",
+        "post_office",
+        ],
+    }
 
 nodes = defaultdict(int)
 ways = defaultdict(int)
 relations = defaultdict(int)
-tags = defaultdict(lambda : defaultdict(int))
-newtags = defaultdict(list)
 
-check = {
-          "highway": [ 
-                       "bus_stop",
-                     ],
-          "amenity": [
-                       "drinking_water",
-                       "pharmacy",
-                       "bank",
-                       "post_office",
-                     ]
-        }
+tags = defaultdict(lambda : defaultdict(int))
+enumtags = defaultdict(list)
+savetags = defaultdict(list)
+
+today = date.today()
+today = "%4d%02d%02d" % (today.year,today.month,today.day)
 
 user = ""
 
@@ -73,13 +83,57 @@ def streamFile(filename):
     if ret:
         print "Failed to parse %s" % (filename)
 
-def mysort(d):
+def mysort(d, enum = True):
     ret = []
     for user in d:
         ret.append([d[user], user])
     ret.sort()
     ret.reverse()
-    return enumerate(ret)
+    if enum:
+	    return enumerate(ret)
+    else:
+        return ret
+
+def save(nodes, ways, relations, tags):
+    try:
+        j = JsonReader()
+        f = open(os.path.expanduser(stats_path), "r")
+        stats = j.read(f.readline())
+        f.close()
+    except (ReadException, IOError):
+        f = open(os.path.expanduser(stats_path), "w")
+        f.close()
+        stats = {}
+        pass
+
+    nodes_count = 0
+    ways_count = 0
+    relations_count = 0
+
+    # let's count primitives
+    for pair in mysort(nodes, False):
+        nodes_count += pair[0]
+    for pair in mysort(ways, False):
+        ways_count += pair[0]
+    for pair in mysort(relations, False):
+        relations_count += pair[0]
+
+    tags_count = defaultdict(int)
+    for tag in tags:
+        for pair in tags[tag]:
+            tags_count[tag] += pair[0]
+
+    stats[today] = {
+        "nodes" : nodes_count,
+        "ways" : ways_count,
+        "relations" : relations_count,
+        "tags" : dict(tags_count),
+    }
+
+    f = open(os.path.expanduser(stats_path), "w")
+    j = JsonWriter()
+    f.write(j.write(stats))
+    f.close()
 
 def main():
     streamFile(sys.argv[1])
@@ -87,16 +141,21 @@ def main():
     tmpl = MarkupTemplate(open("statistiche.tmpl"))
 
     for key in tags:
-        newtags[key] = mysort(tags[key])
+        enumtags[key] = mysort(tags[key])
+        savetags[key] = mysort(tags[key], False)
 
+    save(nodes, ways, relations, savetags)
     stream = tmpl.generate(
-                           nodes = mysort(nodes),
-                           ways = mysort(ways),
-                           relations = mysort(relations),
-                           date = '20090905',
-                           tags = newtags,
-                          )
-    print stream.render('xhtml')
+        nodes = mysort(nodes),
+        ways = mysort(ways),
+        relations = mysort(relations),
+        date = today,
+        tags = enumtags,
+        )
+
+    f = open(html_path, "w")
+    f.write(stream.render("xhtml"))
+    f.close()
 
 if __name__ == '__main__':
     main()
